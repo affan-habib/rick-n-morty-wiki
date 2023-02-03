3f4ef0a9cfe8b3d7ddf7798e54b903ebd6c7bcbc
Affan-Habib <affan.habib44@gmail.com>
2023-02-03 10:52:44 +0600

    home page markup done

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import "./App.css";
 import { useDispatch } from "react-redux";
 import Loader from "./components/Loader";
 import { callApi } from "./reducers/apiSlice";
-
+import logo from "./assets/Logo.png";
+import Stack from "@mui/material/Stack";
+import { Box, Button, Typography } from "@mui/material";
 function App() {
   const dispatch = useDispatch();
   useEffect(() => {
@@ -16,7 +18,25 @@ function App() {
   }, []);
   return (
     <Suspense fallback={<Loader />}>
-      <div className="App">Starting here</div>
+      <div className="App">
+        <Stack alignItems="center" justifyContent="center">
+          <img src={logo} className="logo" />
+        </Stack>
+        <Box>
+          <Typography>The Rick & MORTY WIKI</Typography>
+          <Button variant="outlined">Watch Now</Button>
+          <Typography>
+            Brilliant but boozy scientist Rick hijacks his fretful teenage
+            grandson, Morty, for wild escapades in other worlds and alternate
+            dimensions.
+          </Typography>
+        </Box>
+        <Box mt={4}>
+          <Typography>Meet the cast</Typography>
+          <Typography>Episodes</Typography>
+          <Typography>Locations</Typography>
+        </Box>
+      </div>
     </Suspense>
   );
 }
