4cefa3c7c11a6f871acc073cd165bec614506a49
affan-habib <affan.habib44@gmail.com>
2023-02-07 13:32:42 +0600

    fallback in all charecter in producion build fixed

--- src/pages/characters/AllCharecters.js
+++ src/pages/characters/AllCharecters.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { Suspense, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Loader from "../../components/Loader";
 import { Box, Container, IconButton, Stack, Typography } from "@mui/material";
@@ -35,74 +35,80 @@ const AllCharecters = () => {
     setSearchResults(result);
   }, [searchInput, data]);
   return (
-    <Container maxWidth="xl" m={2}>
-      {loading && <Loader />}
-      <Stack direction="row" justifyContent="space-between" alignItems="center">
-        <Typography variant="h2">
-          {view == "character"
-            ? "The Cast"
-            : view == "location"
-            ? "All Locations"
-            : "Episodes"}
-        </Typography>
-        <Box>
-          <select
-            onChange={(e) => {
-              setView(e.target.value);
-              setPage(1);
-            }}
-          >
-            <option value="character">Charecter</option>
-            <option value="location">Location</option>
-            <option value="episode">Episodes</option>
-          </select>
-          <input
-            type="search"
-            onChange={(e) => setSearchInput(e.target.value)}
-          />
-        </Box>
-      </Stack>
-      <Stack direction="row" flexWrap="wrap" justifyContent="space-between">
-        {searchResults.length == 0 && "No Results"}
-        {searchResults.map((el, index) => (
-          <Box
-            key={index}
-            className="custom-border"
-            sx={{ minWidth: 290, minHeight: 296, mb: 2 }}
-          >
-            {view == "character" && (
-              <Box
-                sx={{ cursor: "pointer" }}
-                onClick={() => navigate(`/character/details/${el.id}`)}
-              >
-                <img src={el.image} width="100%" height="230px" />
-              </Box>
-            )}
-            <Typography>{el.name}</Typography>
+    <Suspense fallback={<Loader />}>
+      <Container maxWidth="xl" m={2}>
+        {loading && <Loader />}
+        <Stack
+          direction="row"
+          justifyContent="space-between"
+          alignItems="center"
+        >
+          <Typography variant="h2">
+            {view == "character"
+              ? "The Cast"
+              : view == "location"
+              ? "All Locations"
+              : "Episodes"}
+          </Typography>
+          <Box>
+            <select
+              onChange={(e) => {
+                setView(e.target.value);
+                setPage(1);
+              }}
+            >
+              <option value="character">Charecter</option>
+              <option value="location">Location</option>
+              <option value="episode">Episodes</option>
+            </select>
+            <input
+              type="search"
+              onChange={(e) => setSearchInput(e.target.value)}
+            />
           </Box>
-        ))}
-      </Stack>
-      <Stack direction="row" justifyContent="center" alignItems="center">
-        <Box py={2}>
-          <span>Page</span>
-          <IconButton
-            disabled={!data.info.prev || page == 1}
-            onClick={() => setPage(page - 1)}
-            color="primary"
-          >
-            <ArrowCircleLeftIcon />
-          </IconButton>
-          <span className="page-number">{page}</span>
-          <IconButton
-            disabled={!data.info.next || page == data.info.pages}
-            onClick={() => setPage(page + 1)}
-          >
-            <ArrowCircleRightIcon />
-          </IconButton>
-          <span>of {data.info.pages}</span>
-        </Box>
-      </Stack>
-    </Container>
+        </Stack>
+        <Stack direction="row" flexWrap="wrap" justifyContent="space-between">
+          {searchResults.length == 0 && "No Results"}
+          {searchResults.map((el, index) => (
+            <Box
+              key={index}
+              className="custom-border"
+              sx={{ minWidth: 290, minHeight: 296, mb: 2 }}
+            >
+              {view == "character" && (
+                <Box
+                  sx={{ cursor: "pointer" }}
+                  onClick={() => navigate(`/character/details/${el.id}`)}
+                >
+                  <img src={el.image} width="100%" height="230px" />
+                </Box>
+              )}
+              <Typography>{el.name}</Typography>
+            </Box>
+          ))}
+        </Stack>
+        <Stack direction="row" justifyContent="center" alignItems="center">
+          <Box py={2}>
+            <span>Page</span>
+            <IconButton
+              disabled={!data.info.prev || page == 1}
+              onClick={() => setPage(page - 1)}
+              color="primary"
+            >
+              <ArrowCircleLeftIcon />
+            </IconButton>
+            <span className="page-number">{page}</span>
+            <IconButton
+              disabled={!data.info.next || page == data.info.pages}
+              onClick={() => setPage(page + 1)}
+            >
+              <ArrowCircleRightIcon />
+            </IconButton>
+            <span>of {data.info.pages}</span>
+          </Box>
+        </Stack>
+      </Container>
+    </Suspense>
   );
 };
 
